4996ee62e836bd3f311d3bf77a70dca486adc01b
sinskiy <sinskiy@proton.me>
2024-06-09 10:33:20 +0300

    allow grid size changing

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const COLUMNS = 16;
-const ROWS = COLUMNS;
+let SIZE = 16;
 
-createRows();
+const container = document.querySelector("#container");
+createRows(container);
 
-function createRows() {
-  for (let i = 0; i < COLUMNS; i++) {
+function createRows(container) {
+  for (let i = 0; i < SIZE; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     createColumn(row);
-    document.body.appendChild(row);
+    container.appendChild(row);
   }
 }
 
 function createColumn(column) {
-  for (let i = 0; i < ROWS; i++) {
+  for (let i = 0; i < SIZE; i++) {
     const itemInColumn = document.createElement("div");
     itemInColumn.classList.add("item");
     itemInColumn.onmouseover = (e) => e.target.classList.add("item--hovered");
@@ -21,3 +21,19 @@ function createColumn(column) {
     column.appendChild(itemInColumn);
   }
 }
+
+function handleGridSizeChange() {
+  const newSize = prompt("Input a new grid size from 1 to 100 (default: 16)");
+  if (newSize >= 1 && newSize <= 100) {
+    SIZE = newSize;
+    currentGridSize.textContent = SIZE;
+  }
+  container.innerHTML = "";
+  createRows(container);
+}
+
+const currentGridSize = document.querySelector("#current-grid-size");
+currentGridSize.textContent = SIZE;
+
+const gridSize = document.querySelector("#grid-size");
+gridSize.addEventListener("click", handleGridSizeChange);
