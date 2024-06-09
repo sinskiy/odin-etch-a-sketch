ac73dd8bd0e2529d0f9eb52e4e68fbb51d6e2b07
sinskiy <sinskiy@proton.me>
2024-06-09 10:02:11 +0300

    feat: add 16x16 grid of divs with test styling

---
+++ script.js
@@ -0,0 +1,21 @@
+const COLUMNS = 16;
+const ROWS = COLUMNS;
+
+createRows();
+
+function createRows() {
+  for (let i = 0; i < COLUMNS; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    createColumn(row);
+    document.body.appendChild(row);
+  }
+}
+
+function createColumn(column) {
+  for (let i = 0; i < ROWS; i++) {
+    const itemInColumn = document.createElement("div");
+    itemInColumn.classList.add("item");
+    column.appendChild(itemInColumn);
+  }
+}
